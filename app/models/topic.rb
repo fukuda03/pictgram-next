ec4ef2ef75eb3c2d4b03670517d92c98d2289a62
shouta <svtd9978@yahoo.co.jp>
2019-03-06 13:12:15 +0900

    画像投稿の課題２

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,12 @@
 class Topic < ApplicationRecord
   validates :user_id, presence: true
   validates :description, presence: true
-  validates :image, presence: true
+  validates :image, presence: true, file_size: { less_than_or_equal_to: 10.megabyte}
 
   belongs_to :user
 
   mount_uploader :image, ImageUploader
+
+
+
 end
